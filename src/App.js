9fa71ffe68ff318910efff1070b42827e7cbf65a
cinthyasegura <cinthyaless@gmail.com>
2019-06-01 14:15:13 -0500

    adding modal and incidents

--- src/App.js
+++ src/App.js
@@ -2,16 +2,14 @@ import React from 'react';
 import ModalTemplate from './modal';
 import './App.css';
 import Incidents from './components/Incidents';
-import StorageFirebase from './components/StorageFirebase';
 
 class App extends React.Component {
   
     render() {
       return (
         <> 
-        <StorageFirebase />
         <Incidents />
-        <ModalTemplate></ModalTemplate> 
+        <ModalTemplate/> 
       </>
     );
   }
--- src/modal.js
+++ src/modal.js
@@ -1,37 +1,77 @@
 import React from 'react';
+import { storage } from './firebase';
+import styled from 'styled-components';
+
+
+const Camera = styled.img`
+  width: 20%;
+`
 
 class ModalTemplate extends React.Component {
-    
+    constructor() {
+        super();
+        this.state = {
+            image: null,
+            url: ''
+        }
+    }
+
+    handleChange = (e) => {
+        if(e.target.files[0]) {
+            const image = e.target.files[0];
+            this.setState(() => ({ image }))
+        }
+    }
+    handleUpload = () => {
+        const {image} = this.state;
+        const uploadTask = storage.ref(`images/${image.name}`).put(image);
+        uploadTask.on('state_changed', 
+        
+        (error) => {
+             // error function ....
+          console.log(error);
+        }, 
+      () => {
+          // complete function ....
+          storage.ref('images').child(image.name).getDownloadURL().then(url => {
+              console.log(url);
+              this.setState({url});
+          })
+      });
+    }
+
     render () {
-           return ( 
+        return ( 
             <>
-                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
-                Incidencias
-                </button>
+    
+        <div className="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div className="modal-dialog modal-dialog-centered" role="document">
+            <div className="modal-content">
+            <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalCenterTitle">Incidencias</h5>
 
+            <div>
+                <label className="mt-5">
+                <input type="file" onChange={this.handleChange} style={{visibility: 'hidden'}}/>
+              <Camera src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZ7fFGqVa3fsdaLgicuPGKh3Bnuhq1hsnFaVNfpJUxOFmzO-PA" alt="camara"/>
+              </label>
+              <button type="button" onClick={this.handleUpload}>Añadir imagen</button>
+            </div>
 
-                <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered" role="document">
-                    <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalCenterTitle">Incidencias</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <div class="form-group">
-                            <label for="exampleFormControlTextarea1">Describe la incidencia</label>
-                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
-                        <button type="button" class="btn btn-primary">Registrar</button>
-                    </div>
-                    </div>
+            </div>
+            <div className="modal-body">
+                <div className="form-group">
+                    <label for="exampleFormControlTextarea1">Describe la incidencia</label>
+                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
                 </div>
-                </div>
+            </div>
+            <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                <button type="button" className="btn btn-primary">Registrar</button>
+            </div>
+            </div>
+        </div>
+        </div>
             </>
            )
         }
