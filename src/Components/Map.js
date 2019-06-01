15654f7aacd2f75ff1c4d3b63b438216f376e268
GabrielaDiazB <gabriela.dbravo@gmail.com>
2019-06-01 14:18:00 -0500

    Se corrigio un error de centrado de zoom del mapa

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -7,19 +7,34 @@ const Wrapper = styled.div`
   width: ${props => props.width};
   height: ${props => props.height};
 `;
+
+const myIcon = L.icon({
+  iconUrl:
+    'https://cdn1.iconfinder.com/data/icons/pixel-perfect-at-16px-volume-2/16/2105-512.png',
+  iconSize: [30, 30],
+  iconAnchor: [30, 30],
+  popupAnchor: [0, -50]
+});
+
 export default class Map extends React.Component {
   constructor() {
     super();
     this.state = {
-      markers: [[-12, -77]]
+      markers: [[-12.04318, -77.02824]]
     };
+  }
+
+  centerLeafletMapOnMarker(map, marker) {
+    const latLngs = [marker.getLatLng()];
+    const markerBounds = L.latLngBounds(latLngs);
+    this.map.fitBounds(markerBounds);
   }
 
   componentDidMount() {
     this.map = L.map('map', {
-      center: [-12.04318, -77.02824],
+      center: [-12.046374, -77.042793],
       zoom: 10,
-      zoomControl: true
+      zoomControl: false
     });
 
     L.tileLayer(
@@ -31,6 +46,11 @@ export default class Map extends React.Component {
         maxNativeZoom: 17
       }
     ).addTo(this.map);
+
+    const marker = L.marker([-12.141667, -77.016667], { icon: myIcon }).addTo(
+      this.map
+    );
+    marker.bindPopup('Alerta de Robo').openPopup();
   }
 
   render() {
