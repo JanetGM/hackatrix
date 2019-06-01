55ea806921061b576c0872f9179b4ad16b408790
maliciacanv <cantenovictoriomaruja@gmail.com>
2019-06-01 11:24:54 -0500

    se maqueteo el archivo loginFacebook

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import  LoginGoogle  from './components/loginGoogle';
+import LoginGoogle  from './components/loginGoogle';
+import LoginFacebook from './components/loginFacebook';
 import styled from 'styled-components';
 
 
@@ -13,6 +14,7 @@ import styled from 'styled-components';
     return (
       <AppContainer>
         <LoginGoogle />
+        <LoginFacebook />
       </AppContainer>
     );
 }
