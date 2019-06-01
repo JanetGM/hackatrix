2618daf54752c846e2a05e653a9c73c53e281617
cinthyasegura <cinthyaless@gmail.com>
2019-06-01 10:14:44 -0500

    adding firebase react and all dependecies

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/storage';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA70kAGHYkedXJien7iQrd_xaGvj4zaptQ",
+    authDomain: "vecinaccion.firebaseapp.com",
+    databaseURL: "https://vecinaccion.firebaseio.com",
+    projectId: "vecinaccion",
+    storageBucket: "vecinaccion.appspot.com",
+    messagingSenderId: "74633213985",
+    appId: "1:74633213985:web:dec377c53fd8b115"
+  };
+  // Initialize Firebase
+  
+  firebase.initializeApp(firebaseConfig);
+
+  export const db = firebase.firestore();
+  export const storage = firebase.storage();
