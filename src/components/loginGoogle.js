2d21757ffcca08b0a3462cc82750a4a6f694ecdd
maliciacanv <cantenovictoriomaruja@gmail.com>
2019-06-01 14:10:49 -0500

    funcionalidad de login con google

--- src/components/loginGoogle.js
+++ src/components/loginGoogle.js
@@ -1,7 +1,53 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import {firebaseAuth} from '../firebase';
+
 
 class LoginGoogle extends Component  {
+
+ constructor(props) {
+   super(props);
+   this.state = {
+     users: {
+       nombre: '',
+       email: '',
+       photoUrl: '',
+       distrito: '',
+       reporte:[{
+         photo: '',
+         comentario: '',
+         tipoDeIncidencia: '',
+         incidenteEspecifico: '',
+         coordenadas: '',
+         fecha: '',
+       }]
+     }
+   }
+ }
+
+currentUser() {
+  const userData = firebaseAuth().currentUser;
+  console.log(userData)
+//   if(userData ==! null){
+
+//    console.log('entro')
+//   }else{
+// console.log('no hay usuario');
+//   }
+
+ 
+
+} 
+
+ signIn(){
+  const provider = new firebaseAuth.GoogleAuthProvider();
+  firebaseAuth().signInWithPopup(provider)
+  .then((result) => {
+    this.currentUser()
+
+  }).catch((error) => { 
+  });
+}
 
     render() {
       const LoginButton = styled.button`
@@ -14,10 +60,12 @@ class LoginGoogle extends Component  {
       border: 1px solid red;
     `;
         return (
+          <>
           <div className="LoginGoogle">
-            <LoginButton type="button" className="btn App-button">
+            <LoginButton type="button" className="btn App-button" onClick={this.signIn}>
               <i className="fab fa-google-plus-g"></i> Iniciar sesión con google</LoginButton>
           </div>
+          </>
         );      
     }
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
 import firebase from 'firebase/app';
+import 'firebase/auth';
 import 'firebase/firestore';
 import 'firebase/storage';
 
@@ -12,8 +13,8 @@ const firebaseConfig = {
     appId: "1:74633213985:web:dec377c53fd8b115"
   };
   // Initialize Firebase
-  
   firebase.initializeApp(firebaseConfig);
 
   export const db = firebase.firestore();
   export const storage = firebase.storage();
+  export const firebaseAuth = firebase.auth;
