dbb2716532a27b08b0e34dd94fdc1ffd0ec0a44b
JanetGM <antaly.146@gmail.com>
2019-06-05 09:48:58 -0500

    Updates

---
+++ precache-manifest.e5e1f6e46246ef37d02c9537b97b0fb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5552139f1e02a3be7aa51e8a31c74f1f",
+    "url": "/Reto-Rimac/index.html"
+  },
+  {
+    "revision": "333c7039b15d952dffed",
+    "url": "/Reto-Rimac/static/css/2.098bc035.chunk.css"
+  },
+  {
+    "revision": "07947515d15ff264a0e5",
+    "url": "/Reto-Rimac/static/css/main.6e1fdd3a.chunk.css"
+  },
+  {
+    "revision": "333c7039b15d952dffed",
+    "url": "/Reto-Rimac/static/js/2.10d23ec6.chunk.js"
+  },
+  {
+    "revision": "07947515d15ff264a0e5",
+    "url": "/Reto-Rimac/static/js/main.d4cda632.chunk.js"
+  },
+  {
+    "revision": "5b39d12ed81448e2fa99",
+    "url": "/Reto-Rimac/static/js/runtime~main.4e6a8d30.js"
+  }
+]);
